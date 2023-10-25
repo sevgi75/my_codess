c1f2474acad767c5280bbbbef8905cf22f2ee7b7
Sevgi <erbilsevg97@gmail.com>
2023-10-25 20:26:34 +0300

    destructuring

--- javascript/js11_new-gen-operators/newOps.js
+++ javascript/js11_new-gen-operators/newOps.js
@@ -4,6 +4,51 @@ console.log("** NEW OPERATORS **")
 //*  DESTRUCTURING (OBJECT)
 //* ======================================================
 
+const insan = {
+    kimlikNO: "1234567890",
+    adi: "Ahmet",
+    soyadi: "Can",
+    meslek: "Mimar",
+    maas: 30000,
+}
+
+console.log(insan.maas) //? . notasyonun
+console.log(insan["meslek"]) //? square bracket
+
+const mimarAdi = insan.adi
+console.log(mimarAdi)
+
+//?Destructure
+let { adi, soyadi, kimlikNo, maas } = insan
+
+console.log(kimlikNo)
+kimlikNo = "ABACD"
+console.log(kimlikNo, maas, adi, soyadi)
+
+insan.meslek = "Muhendis"
+console.log(insan)
+
+//? NESTED
+
+const insanlar = {
+  kisi1: {
+    kimlikNo: "1234567890",
+    adi: "Ahmet",
+    soyadi: "Can",
+    meslek: "Mimar",
+    maas: 30000,
+  },
+  kisi2: {
+    kimlikNo: "44234567890",
+    adi: "Canan",
+    soyadi: "Can",
+    meslek: "Sosyal Gelişim Uzmanı",
+    maas: 25000,
+  },
+}
+
+
+
 //* ======================================================
 //*  DESTRUCTURING (ARRAY)
 //* ======================================================
