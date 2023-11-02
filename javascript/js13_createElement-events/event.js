60082ab3966812495b3e8b8e00c9b6cceecf3712
sevgi75 <erbilsevgi97@gmail.com>
2023-11-02 22:39:44 +0300

    focus

--- javascript/js13_createElement-events/event.js
+++ javascript/js13_createElement-events/event.js
@@ -3,3 +3,36 @@
 //* ===============================================
 
 console.log("****** EVENTS *******")
+
+const header = document.querySelector("header")
+const addButton = document.getElementById("btn")
+
+//? 3. yontemle event tanimlandi
+//* mouse header 'ın üzerine geldiginde ilgili function cagrilir.
+header.onmouseover = function () {
+  header.style.backgroundColor = "red"
+  header.style.color = "white"
+}
+//* mouse header 'ın üzerinden ayrildiginda ilgili function cagrilir.
+header.onmouseout = function () {
+  header.style.backgroundColor = "transparent"
+  header.style.color = "black"
+}
+
+//? 4.Yontem (addeventlistener metodu) ile event tanimlandi
+//* add butonuna tiklanildgin event funksiyonu cagrilir
+addButton.addEventListener("click", () => {
+    alert("Buton tiklanildi")
+  })
+  
+  const yazdir = () => {
+    document.write("Hello from Turkey")
+  }
+  
+  //? onload event'ı html ve css kodlarininin render edilmesinin hemen akabinde calisir.
+  window.onload = yazdir()
+  
+  window.addEventListener("load", () => {
+    document.querySelector("#input").focus()
+  })
+  
