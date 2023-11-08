aebb9153043ae7e474c068ae6c2b7925de39ee09
sevgi75 <erbilsevgi97@gmail.com>
2023-11-08 19:04:42 +0300

    plus

--- javascript/js15_checkout-app/checkout.js
+++ javascript/js15_checkout-app/checkout.js
@@ -2,14 +2,53 @@
 //*        CHECKOUT APP
 //***********************************/
 
+// function createEventsForPlusMinus() {
+//     const minusBtns = document.querySelectorAll(".fa-minus")
+//     const plusBtns = document.querySelectorAll(".fa-plus")
+    
+//     minusBtns.forEach((minus) =>{
+//         minus.addEventListener("click", () => {
+//             //? Eksilme islemler
+//         })
+//     })
+
+//     plusBtns.forEach((plus) => {
+//         plus.addEventListener("click", () => {
+//             //? Artirma islemleri
+//         })
+//     })
+// }
+
 const deleteProducts = document.querySelector(".delete-div .fa-trash-can")
 const products = document.querySelector(".products")
 
-deleteProducts.addEventListener("click", () => {
+deleteProducts.addEventListener("click", (e) => {
     if (confirm("Silmek istedigine eminmisiniz")) {
         products.textContent = "No product"
         products.classList.add("no-product")
-        document.querySelector(".delete-div").style.display = "none"
+        // document.querySelector(".delete-div").style.display = "none"
+        e.target.parentElement.style.display = "none"
     }
 
+})
+
+// products.addEventListener("click", (e) => {
+//     console.log(e.target);
+//     if (e.target.classList.contains("fa-minus")) {
+//         alert("minus")
+//     }else if (e.target.classList.contains("fa-plus")) {
+//         alert("plus")
+//     } else if (e.target.classList.contains("fa-trash-can")) {
+//         alert("remove")
+//     } else {
+//         alert("diger")
+//     }
+// })
+
+
+products.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (e.target.classList.contains("fa-plus")) {
+        
+    }
 })
