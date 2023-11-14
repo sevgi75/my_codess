eeb602c4afbb510b9cb514ad3806f1c3adb7d923
sevgi75 <erbilsevgi97@gmail.com>
2023-11-14 18:50:57 +0300

    ekle formu

---
+++ javascript/js16_wallet-app/app.js
@@ -0,0 +1,29 @@
+
+// selectors
+
+const ekleBtn = document.getElementById("ekle-btn")
+const gelirInput = document.getElementById("gelir-input")
+const ekleFormu = document.getElementById("ekle-formu")
+
+// variables
+
+let gelirler = 0;
+
+// Hesap tablosu
+const gelirinizTd = document.getElementById("geliriniz")
+const giderinizTd = document.getElementById("gideriniz")
+const kalanTd = document.getElementById("kalan")
+
+
+// Ekle formu
+
+ekleFormu.addEventListener("click", (e) => {
+    e.preventDefault()
+    gelirler = gelirler + Number(gelirInput.value)
+    console.log(gelirler);
+    localStorage.setItem("gelirler", gelirler)
+    gelirinizTd.innerText = gelirler
+    ekleFormu.reset()
+})
+
+
