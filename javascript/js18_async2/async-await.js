2f291c35ce9b1cc7ef5dc6aa0d2a468a812f3f1b
sevgi75 <erbilsevgi97@gmail.com>
2023-11-18 19:58:17 +0300

    async-await

--- javascript/js18_async2/async-await.js
+++ javascript/js18_async2/async-await.js
@@ -17,3 +17,28 @@
 //* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
 //* satirdaki kodun durdurulmasini saglar.
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
+
+// https://newsapi.org/v2/top-headlines?country=tr&category=sport&apiKey=4c8668fd0dd8484f806177962a8c7b82
+
+const getNews = async () => {
+
+    const API_KEY = "4c8668fd0dd8484f806177962a8c7b82"
+    const BASE_URL = "https://newsapi.org/v2/"
+    const queryString = "top-headlines?country=de&category=sport"
+
+    // const res = await fetch(`https://newsapi.org/v2/top-headlines?country=tr&category=sport&apiKey=4c8668fd0dd8484f806177962a8c7b82`)
+
+    try {
+        const res = await fetch(`${BASE_URL}${queryString}&apiKey=${API_KEY}`)
+    const data = await res.json()
+    console.log(data.articles);
+    } catch (error) {
+        console.log(error);        
+    }   
+
+
+}
+
+window.addEventListener("load", () => {
+    getNews()
+})
