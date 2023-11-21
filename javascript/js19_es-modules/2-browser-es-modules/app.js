3920213490397a9062cf1552d50232c66e156684
sevgi75 <erbilsevgi97@gmail.com>
2023-11-21 17:48:08 +0300

    export import

--- javascript/js19_es-modules/2-browser-es-modules/app.js
+++ javascript/js19_es-modules/2-browser-es-modules/app.js
@@ -6,7 +6,17 @@
 //? JS kodlarının kolay ve performansli bir şekilde farklı dosyalara ayrilmasina ve gerektiginde birleştirmesine olanak saglamaktadir.
 //? Bir degisken,dizi,obje, fonksiyon, class v.b "export" deyimi ile başka dosyalarinin kullanimina açilmabilir.
 //? Kullanima acilanlar ise import deyimi ile istenilen dosyaya eklenebilir.
+import { isim, PI as pi } from "./hesapMakinasi.js";
+// import { PI } from "./hesapMakinasi.js";
 
-console.log("***ES-Modules**");
+console.log(isim);
+console.log(pi * 10 * 10);
+
+import {topla,cikar,carp,bol} from "./hesapMakinasi.js" //importlar hoisted olurlar
 
 console.log("TOPLAM:", topla(3, 4));
+console.log("Bol:", bol(6, 4));
+console.log("Carp:", carp(5, 4));
+console.log("Cikar:", cikar(5, 4));
+
+//! 1- importlar hoisted olur bu yuzden herhangi bir yerde eklenebilir ancak best practice en uste eklenmeleridir.
--- javascript/js19_es-modules/2-browser-es-modules/hesapMakinasi.js
+++ javascript/js19_es-modules/2-browser-es-modules/hesapMakinasi.js
@@ -3,15 +3,17 @@
 //*************************************/
 
 //? named export
-const isim = "-- Hesap Makinasi--"
+export const isim = "-- Hesap Makinasi--"
 
-const PI = 3.1415
-const asalSayilar = [2, 3, 5, 7, 11, 13, 17, 23]
+export const PI = 3.1415
+export const asalSayilar = [2, 3, 5, 7, 11, 13, 17, 23]
 
 const topla = (a, b) => a + b
 const cikar = (a, b) => a - b
 const carp = (a, b) => a * b
 const bol = (a, b) => a / b
+
+export {topla,cikar,carp,bol}
 
 const mesaj = () => {
     console.log("Module Bitti");
