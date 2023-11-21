6a16bc8f0b5725fc21bf2e40112a6609fb4bcb44
sevgi75 <erbilsevgi97@gmail.com>
2023-11-21 15:55:56 +0300

    app js

---
+++ javascript/js19_es-modules/1-without-modules/app.js
@@ -0,0 +1,25 @@
+console.log("APP JS");
+
+const h1 = document.querySelector("h1")
+
+h1.textContent = "Without module"
+
+const users = ["Ahmet", "Mehmet", "İsmet", "Saffet"]
+
+const products = [
+    {
+      phone: "Iphone",
+      type: "15Pro",
+      price: 50000,
+    },
+    {
+      phone: "Samsung",
+      type: "S23",
+      price: 40000,
+    },
+    {
+      phone: "Huawei",
+      type: "Mate60",
+      price: 45000,
+    },
+  ]
