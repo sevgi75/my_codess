29653c786a8d767a0c552b9e24c2d51fc00cd037
sevgi75 <erbilsevgi97@gmail.com>
2023-11-24 14:51:48 +0300

    form

--- javascript/js20_coinApp/coin-app-inClass/main.js
+++ javascript/js20_coinApp/coin-app-inClass/main.js
@@ -1,3 +1,12 @@
 //? Projenin style dosyasini ana js(main.js) kodumuza import ettik.
 import "./scss/style.scss"
 
+//? Sellector
+const form = document.querySelector("header form")
+
+//? Form icin submit event'nin tanimlanmasi
+form.addEventListener("submit", (e) => {
+  console.log(e.target);
+  e.preventDefault() //?tum sabmit davranislarini engelle
+  e.target.reset()  //? formu silme davranisi calistir
+})
