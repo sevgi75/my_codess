caeb0834c5f3d917b67b14d4e6eb753a23cec4ed
sevgi75 <erbilsevgi97@gmail.com>
2023-11-24 17:53:04 +0300

    dist

--- javascript/js20_coinApp/coin-app-inClass/main.js
+++ javascript/js20_coinApp/coin-app-inClass/main.js
@@ -1,12 +1,24 @@
 //? Projenin style dosyasini ana js(main.js) kodumuza import ettik.
 import "./scss/style.scss"
+import { apiRequest } from "./src/apiRequest.js";
+
+
 
 //? Sellector
 const form = document.querySelector("header form")
 
 //? Form icin submit event'nin tanimlanmasi
-form.addEventListener("submit", (e) => {
-  console.log(e.target);
+form.addEventListener("submit", (e) => {  
   e.preventDefault() //?tum sabmit davranislarini engelle
+  getCoinData()  //? API' ye istek at
   e.target.reset()  //? formu silme davranisi calistir
 })
+
+const getCoinData = () => {
+  const input = document.querySelector("header form input").value
+  if (!input.trim()) {
+    alert("Input can not be blank")    
+  }else {
+    apiRequest(input)
+  }  
+}
---
+++ javascript/js20_coinApp/coin-app-inClass/src/renderData.js
@@ -0,0 +1,32 @@
+export const renderCoins = (coin) => {
+    const { price, name, iconUrl, change, symbol, rank } = coin
+    const coinsUl = document.querySelector("main .coins")
+
+    const newCoinLi = document.createElement("li")
+    newCoinLi.classList.add("coin")
+    newCoinLi.innerHTML = `
+    <div class="remove-icon">
+        <i class="fas fa-window-close"></i>
+    </div>
+    <h2 class="coin-name">
+        <span>${name}</span>
+        <sup>${symbol}</sup>
+    </h2>
+    <div class="coin-temp">$${Number(price).toFixed(4)}</div>
+    <figure>
+        <img src="${iconUrl}" alt="">
+        <figcaption style='color:${change < 0 ? "red" : "green"}'>
+            <i class="fa-solid fa-chart-line"></i>
+            <span>${change || "0"}%</span>
+        </figcaption>
+        <div>RANK:${rank}</div>
+    </figure>    
+    `
+    //? Olusan li elementini ul listesine bagladik
+    coinsUl.append(newCoinLi)
+
+    //? Sil butonu icin event tanimlanmasi
+    newCoinLi.querySelector(".remove-icon").addEventListener("click", () => {
+        newCoinLi.remove()
+    })
+}
