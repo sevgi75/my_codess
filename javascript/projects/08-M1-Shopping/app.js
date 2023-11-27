0c7a0616462428086831f36e10fb54d471c77382
sevgi75 <erbilsevgi97@gmail.com>
2023-11-27 15:42:04 +0300

    quantity

--- javascript/projects/08-M1-Shopping/app.js
+++ javascript/projects/08-M1-Shopping/app.js
@@ -15,6 +15,7 @@ const btnColors = [
 ]
 
 let products = []
+let baskets = []
 
 const getProducts = async () => {
     const res = await fetch("https://anthonyfs.pythonanywhere.com/api/products/")
@@ -22,6 +23,7 @@ const getProducts = async () => {
     console.log(data);
     products = data
     category()
+    displayProducts(products)
 }
 getProducts()
 const category = () => {
@@ -55,3 +57,53 @@ const category = () => {
         btnDivs.appendChild(btn)
     })
 }
+
+function displayProducts(arr) {
+    productDivs.innerHTML = "";
+    arr.forEach((item) => {
+      const { id, title, description, price, image } = item;
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("col");
+      productDiv.setAttribute("id", id);
+      productDiv.innerHTML = `
+          <div class="card">
+              <img src="${image}" class="p-2" height="250px" alt="...">
+              <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+                <p class="card-text line-clamp-2">${description}</p>
+              </div>
+              <div class="card-footer w-100 fw-bold d-flex justify-content-between gap-3">
+              <span>Price:</span><span>${price} $</span>
+                  
+              </div>
+              <div class="card-footer w-100 d-flex justify-content-center gap-3">
+                  <button class="btn btn-danger">
+                  Sepete Ekle
+                  </button>
+                  <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  See Details
+                  </button>
+              </div>
+            </div>
+          `;
+      productDiv.addEventListener("click", (e) => {
+        if (e.target.classList.contains("btn-danger")) {
+          addToCart(item);
+        }else if (e.target.classList.contains("btn-primary")){
+          showModal(item)
+        }
+      });
+      productDivs.appendChild(productDiv);
+    });
+  }
+  function addToCart(product) {
+    console.log(product);
+    if (baskets.some(item => item.title === product.title)) {
+        baskets = baskets.map(item => {
+            return item.id === product.id ? {...item,quantity:item.quantity+1} : item
+        })
+    }else {
+        baskets.push(product)
+    }
+    console.log(baskets);
+  }
